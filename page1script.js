addb017aca9e323cef86d13fc68a1af2f99bde8e
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-02 14:22:37 +0200

    fun voor alle letters achter elkaar toetevoegen

--- page1script.js
+++ page1script.js
@@ -1,6 +1,7 @@
 var list_a = [];
 var totaal_list_a = 0;
 var list_b = [];
+var totaal_list_b = "";
 var list_c = [];
 var list_d = [];
 
@@ -10,12 +11,11 @@ function a(a,b , c ,d){
     list_c.push(c);
     list_d.push(d);
 
-    for(i = 1; i <= list_a.length ; i ++){
-        totaal_list_a += list_a[0];
-    }
+    totaal_list_a += list_a[list_a.length -1];
+    totaal_list_b += list_b[list_b.length -1];
 
     console.log(totaal_list_a);
-    console.log(list_b[0]);
-    console.log(list_c[0]);
-    console.log(list_d[0]);
+    console.log(totaal_list_b);
+    //console.log(list_c[0]);
+    //console.log(list_d[0]);
 }

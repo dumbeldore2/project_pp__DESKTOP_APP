d2859346e4ac8f1fd571c98131da68760e872db0
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-02 14:10:03 +0200

    fun a heeft een manier om list a volledig op te tellen

--- page1script.js
+++ page1script.js
@@ -1,4 +1,5 @@
 var list_a = [];
+var totaal_list_a = 0;
 var list_b = [];
 var list_c = [];
 var list_d = [];
@@ -9,8 +10,11 @@ function a(a,b , c ,d){
     list_c.push(c);
     list_d.push(d);
 
+    for(i = 1; i <= list_a.length ; i ++){
+        totaal_list_a += list_a[0];
+    }
 
-    console.log(list_a[0]);
+    console.log(totaal_list_a);
     console.log(list_b[0]);
     console.log(list_c[0]);
     console.log(list_d[0]);

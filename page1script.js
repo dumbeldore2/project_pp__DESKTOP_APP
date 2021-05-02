f5150660d1db41decd8a7de5072b358b5ee0683b
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-02 14:32:41 +0200

    page 1 is aangepast zodat de titel ook een href is geworden

--- page1script.js
+++ page1script.js
@@ -4,6 +4,7 @@ var list_b = [];
 var totaal_list_b = "";
 var list_c = [];
 var list_d = [];
+var key = false;
 
 function a(a,b , c ,d){
     list_a.push(a);
@@ -14,8 +15,13 @@ function a(a,b , c ,d){
     totaal_list_a += list_a[list_a.length -1];
     totaal_list_b += list_b[list_b.length -1];
 
-    console.log(totaal_list_a);
-    console.log(totaal_list_b);
+    //console.log(totaal_list_a);
+    //console.log(totaal_list_b);
     //console.log(list_c[0]);
     //console.log(list_d[0]);
+
+    if(totaal_list_a == 30){key = true};
+    if(totaal_list_b == "PP"){key = true};
+    if(totaal_list_b == "HeP"){key = true};
+    console.log(key);
 }

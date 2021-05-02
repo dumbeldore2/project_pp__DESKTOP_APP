571d7d7878beeea992abf6cdb5a9008eb2d086b7
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-02 14:04:23 +0200

    test van de fun a verbreding

--- page1script.js
+++ page1script.js
@@ -1,3 +1,17 @@
+var list_a = [];
+var list_b = [];
+var list_c = [];
+var list_d = [];
+
 function a(a,b , c ,d){
-    console.log(a , b , c , d);
+    list_a.push(a);
+    list_b.push(b);
+    list_c.push(c);
+    list_d.push(d);
+
+
+    console.log(list_a[0]);
+    console.log(list_b[0]);
+    console.log(list_c[0]);
+    console.log(list_d[0]);
 }

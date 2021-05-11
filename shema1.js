77b2686843e3380665dc8da618a56c3eec0636a1
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-11 22:30:22 +0200

    shema1 correct gemaakt

--- shema1.js
+++ shema1.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema({
-    naam: String,
-    plaats: Number,
-    date: String
+    id: Number,
+    account: String,
+    date: String,
+    object: String,
+    email: String,
+    password: String
 })
 
 module.exports = mongoose.model('shema',schema)

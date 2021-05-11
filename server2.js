d5fa3f7fdfee025ba79f2cfe40f72b254455f8fb
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-11 22:24:19 +0200

    path is aangepast

--- server2.js
+++ server2.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const path = 'mongodb+srv://DUMBELDORE:Judolessen12@netflixuserdb.jfos1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const path = 'mongodb+srv://DUMBELDORE:Judolessen12@netflixuserdb.jfos1.mongodb.net/project_pp?retryWrites=true&w=majority'
 
 module.exports = async () => {
     await mongoose.connect(path ,{

216aa58345e9b77cc1f9623afcd5a8d0c6bb4da1
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-11 22:39:03 +0200

    conecteerd met de db

--- server1.js
+++ server1.js
@@ -1,1 +1,30 @@
-const Mongoose = require('mongoose');
+const Mongoose = require('mongoose')
+const mongo = require('./server2')
+const shema = require('./shema1')
+
+
+const connectToMongoDb = async () => {
+    await mongo().then(async (Mongoose) => {
+        try {
+
+            const dbData = await shema.find({});
+            //console.log(object.date)
+            console.log(dbData.slice(-1).length)
+
+            //list.naam = dbData.slice(-1)[0].naam;
+            //list.date = dbData.slice(-1)[0].date;
+        
+            //console.log(list)
+            //var dataCombo = JSON.stringify(list);
+            //fs.writeFileSync('./combo.json',dataCombo,finish)
+
+            function finish(err){
+                console.log("gg")
+            }
+        } finally {
+            Mongoose.connection.close(); 
+        }
+    })
+}
+
+connectToMongoDb();

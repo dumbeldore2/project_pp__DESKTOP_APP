e61347c5b79b658cb3345050cd1cc749a1cfc323
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-13 23:40:10 +0200

    er is een list met de vier inputs

--- page3script.js
+++ page3script.js
@@ -1,7 +1,17 @@
 function a(){
+
+    var b = {};
+
     console.log("lol ik ben een fucking legend");
     console.log(document.getElementById('name').value);
     console.log(document.getElementById('email').value);
     console.log(document.getElementById('password').value);
     console.log(document.getElementById('img').value);
+
+    b.naam = document.getElementById('name').value;
+    b.email = document.getElementById('email').value;
+    b.password = document.getElementById('password').value;
+    b.img = document.getElementById('password').value;
+
+    console.log(b);
 }

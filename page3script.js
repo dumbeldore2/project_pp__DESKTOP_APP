58dd100dd449485a18df01a17c8b3f208de09230
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-13 23:26:01 +0200

    vijf quiries zijn er nu

--- page3script.js
+++ page3script.js
@@ -1,4 +1,7 @@
 function a(){
     console.log("lol ik ben een fucking legend");
     console.log(document.getElementById('name').value);
+    console.log(document.getElementById('email').value);
+    console.log(document.getElementById('password').value);
+    console.log(document.getElementById('img').value);
 }

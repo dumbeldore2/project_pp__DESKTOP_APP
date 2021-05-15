83c508d5151ed6f63170c60de8afbcce71db9263
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-15 22:04:28 +0200

    server 1 ziet de json file

--- server3.js
+++ server3.js
@@ -1,7 +1,10 @@
 const Mongoose = require('mongoose')
 const mongo = require('./server2')
 const shema = require('./shema1')
+const fs = require('fs');
 
 var data = fs.readFileSync('./json1newitems.json');
 var json1 = {};
 json1 = JSON.parse(data);
+
+console.log(json1);

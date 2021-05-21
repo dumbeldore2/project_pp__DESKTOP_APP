2b6285d70783a68d5fa89fa03d090e410cc8b2de
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-05-21 02:15:49 +0200

    classes zijn geadd aan de javascript

--- page2script.js
+++ page2script.js
@@ -5,7 +5,7 @@ require('electron').ipcRenderer.on('final_object',(event,message) => {
     final_object = message;
     console.log(final_object.length);
 
-    var ul = document.querySelector(".ul2");
+    var ul = document.querySelector(".ul1");
 
     for(i = 0 ; i < final_object.length ; i ++){
 
@@ -13,9 +13,19 @@ require('electron').ipcRenderer.on('final_object',(event,message) => {
 
         var li = document.createElement("li");
         var a = document.createElement("a");
-        var div = document.createElement("div");
-        var p = document.createElement("p");
+        a.setAttribute("href","page4.html");
 
+        var div = document.createElement("div");
+        div.setAttribute("class","object");
+
+        var p = document.createElement("p");
+        p.setAttribute("class","object_title");
+
+        var img = document.createElement("img");
+        img.setAttribute("src","facebook.png");
+        img.setAttribute("class","object_img");
+
+        div.appendChild(img);
         div.appendChild(p);
         a.appendChild(div);
         li.appendChild(a);
